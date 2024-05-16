3dd1bdd699cf4712af4f2cc5c7f3df9dd6a96216
MaximeZpn <maxime.zoppini@eleve.leschartreux.net>
2024-05-16 09:15:41 +0200

    ajout cactus

--- src/Model/Obstacle.java
+++ src/Model/Obstacle.java
@@ -6,13 +6,18 @@ public class Obstacle {
     private final int width;
     private final int height;
     private final int GROUND_LEVEL = 300;
+    private final int MIN_SPACE = 100; // Minimum space between obstacles
+    private final int MAX_SPACE = 300; // Maximum space between obstacles
 
     public Obstacle(int xPosition) {
         this.xPosition = xPosition;
-        this.width = 20;  // Largeur fixe pour simplifier
-        this.height = 40; // Hauteur fixe pour simplifier
-        this.yPosition = GROUND_LEVEL - height; // Position en y basée sur la hauteur de l'obstacle et le niveau du sol
+        this.width = 20;  // Fixed width for simplicity
+        this.height = 40; // Fixed height for simplicity
+        this.yPosition = GROUND_LEVEL - height; // Y position based on obstacle height and ground level
+        int space = (int) (Math.random() * (MAX_SPACE - MIN_SPACE + 1)) + MIN_SPACE; // Randomly generate space between obstacles
+        this.xPosition += space + 100; // Add space to x position
     }
+        
 
     public int getXPosition() {
         return xPosition;
--- src/Vue/GameView.java
+++ src/Vue/GameView.java
@@ -1,22 +1,24 @@
 package Vue;
 
-import Model.GameModel;
-import Model.TRex;
-import Model.Obstacle;
-import javax.swing.JPanel;
-import javax.swing.ImageIcon;
-import java.awt.Graphics;
-import java.util.List;
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Dimension; // Import the Dimension class
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Image;
-import java.io.File;
+import java.util.List;
+
+import javax.swing.ImageIcon; // Import the ImageIcon class
+import javax.swing.JPanel;
+
+import Model.GameModel;
+import Model.Obstacle;
+import Model.TRex;
 
 public class GameView extends JPanel {
     private GameModel model;
     private final int GROUND_LEVEL = 300; // Correspond au sol dans TRex
     private Image trexImage; // Déclarer l'image du T-Rex
+    private Image obstacleImage; 
 
     public GameView(GameModel model) {
         this.model = model;
@@ -25,8 +27,18 @@ public class GameView extends JPanel {
         setBackground(Color.BLUE); // Changer la couleur de fond
 
         // Charger l'image du T-Rex
-        ImageIcon ii = new ImageIcon("C:\\Users\\HP\\Documents\\Cours\\CPE\\Deuxième semestre\\POO\\shark\\projet-t-rex\\src\\Vue\\teqshark.png"); // Chemin relatif
-        trexImage = ii.getImage();
+        ImageIcon trexIcon = new ImageIcon("src/Vue/teqshark.png");
+        trexImage = trexIcon.getImage();
+        if (trexImage == null) {
+            System.out.println("Erreur : l'image teqshark.png n'a pas été chargée.");
+        }
+
+        // Charger l'image de l'obstacle
+        ImageIcon obstacleIcon = new ImageIcon("src/Vue/cactus.png");
+        obstacleImage = obstacleIcon.getImage();
+        if (obstacleImage == null) {
+            System.out.println("Erreur : l'image cactus.png n'a pas été chargée.");
+        }
     }
 
     public GameModel getModel() {
@@ -42,11 +54,11 @@ public class GameView extends JPanel {
         g.drawImage(trexImage, tRex.getXPosition(), tRex.getYPosition(), tRex.getWidth(), tRex.getHeight(), this);
 
         List<Obstacle> obstacles = model.getObstacles();
-        g.setColor(Color.RED);
         for (Obstacle obstacle : obstacles) {
-            g.fillRect(obstacle.getXPosition(), obstacle.getYPosition(), obstacle.getWidth(), obstacle.getHeight()); // Dessin des obstacles
+            // Dessiner l'image de l'obstacle
+            g.drawImage(obstacleImage, obstacle.getXPosition(), obstacle.getYPosition(), obstacle.getWidth(), obstacle.getHeight(), this);
         }
-
+        
         // Dessiner le sol
         g.setColor(Color.BLACK);
         g.fillRect(0, GROUND_LEVEL, getWidth(), 5); // Ligne représentant le sol
